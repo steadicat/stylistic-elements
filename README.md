ac7008c26efd9fde0400a8de1592fc4dc2971a60
Stefano J. Attardi <stefano@storehouse.co>
2015-08-16 22:53:48 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Stylistic Elements
+
+A set of React components that make it easy to style elements using props.
+
+For example:
+
+```js
+import {Element} from 'stylistic-elements';
+
+<Element
+  tag="input"
+  type="text"
+  id"myElement"
+  onClick={onClickHandler}
+  marginTop={12}
+  translateY={24}
+/>
+
+// Renders as:
+
+<input
+  type="text"
+  id="myElement"
+  onClick={onClickHandler}
+  style={{
+    marginTop: 12,
+    transform: 'translate(0, 24px)',
+  }}
+/>
+```
+
+## Available elements
+
+- `Element`: the most basic one, simply converts props into inline styles. I recommend using one of the higher-level components below instead.
+- `ResetElement`: like `Element` but resets some common user-agent styles to their defaults.
+- `Block`: a block-level element. Defaults to `div`.
+- `Inline`: an inline element. Defaults to `span`.
+- `InlineBlock`: an inline-block element. Defaults to a `div` with `display: inline-block` and `vertical-align: middle`.
+
+## Supported attributes
+
+See [stylistic](https://github.com/steadicat/stylistic) for the list of supported attributes.
+
---
+++ src/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {extract} from 'stylistic';
+
+export class Element {
+  static displayName = 'Element';
+
+  render() {
+    const attributes = extract(this.props);
+
+    const {
+      tag: Tag='div',
+      canvasHeight,
+      canvasWidth,
+      style,
+      ...plainAttributes,
+    } = attributes;
+
+    return (
+      <Tag
+        style={style}
+        width={canvasWidth}
+        height={canvasHeight}
+        {...plainAttributes}
+      />
+    );
+  }
+}
+
+export class ResetElement {
+  static displayName = 'ResetElement';
+  render() {
+    return (
+        <Element
+        color="inherit"
+        fontFamily="inherit"
+        fontWeight="inherit"
+        fontSize="inherit"
+        textDecoration="none"
+        marginTop={0}
+        marginRight={0}
+        marginBottom={0}
+        marginLeft={0}
+        paddingTop={0}
+        paddingRight={0}
+        paddingBottom={0}
+        paddingLeft={0}
+        {...this.props}
+      />
+    );
+  }
+}
+
+export class Block {
+  static displayName = 'Block';
+  render() {
+    return <Element display="block" {...this.props} />
+  }
+}
+
+export class Inline {
+  static displayName = 'Inline';
+  render() {
+    return <Element tag="span" {...this.props} />
+  }
+}
+
+export class InlineBlock {
+  static displayName = 'InlineBlock';
+  render() {
+    return <Element display="inline-block" verticalAlign="middle" {...this.props} />
+  }
+}
