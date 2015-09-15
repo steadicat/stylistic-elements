01b9c6c1b51886ee9e33e53999595020a83d9b13
Stefano J. Attardi <stefano@storehouse.co>
2015-09-15 13:32:33 -0700

    React 0.14-rc1 compatibility

--- src/index.js
+++ src/index.js
@@ -1,72 +1,56 @@
 import React from 'react';
 import {extract} from 'stylistic';
 
-export class Element {
-  static displayName = 'Element';
+export function Element(props) {
+  const attributes = extract(props);
 
-  render() {
-    const attributes = extract(this.props);
+  const {
+    tag: Tag='div',
+    canvasHeight,
+    canvasWidth,
+    style,
+    ...plainAttributes,
+  } = attributes;
 
-    const {
-      tag: Tag='div',
-      canvasHeight,
-      canvasWidth,
-      style,
-      ...plainAttributes,
-    } = attributes;
-
-    return (
-      <Tag
-        style={style}
-        width={canvasWidth}
-        height={canvasHeight}
-        {...plainAttributes}
-      />
-    );
-  }
+  return (
+    <Tag
+      style={style}
+      width={canvasWidth}
+      height={canvasHeight}
+      {...plainAttributes}
+    />
+  );
 }
 
-export class ResetElement {
-  static displayName = 'ResetElement';
-  render() {
-    return (
-      <Element
-        color="inherit"
-        fontFamily="inherit"
-        fontWeight="inherit"
-        fontSize="inherit"
-        textDecoration="none"
-        marginTop={0}
-        marginRight={0}
-        marginBottom={0}
-        marginLeft={0}
-        paddingTop={0}
-        paddingRight={0}
-        paddingBottom={0}
-        paddingLeft={0}
-        {...this.props}
-      />
-    );
-  }
+export function ResetElement(props) {
+  return (
+    <Element
+      color="inherit"
+      fontFamily="inherit"
+      fontWeight="inherit"
+      fontSize="inherit"
+      textDecoration="none"
+      marginTop={0}
+      marginRight={0}
+      marginBottom={0}
+      marginLeft={0}
+      paddingTop={0}
+      paddingRight={0}
+      paddingBottom={0}
+      paddingLeft={0}
+      {...props}
+    />
+  );
 }
 
-export class Block {
-  static displayName = 'Block';
-  render() {
-    return <Element display="block" {...this.props} />
-  }
+export function Block(props) {
+  return <Element display="block" {...props} />;
 }
 
-export class Inline {
-  static displayName = 'Inline';
-  render() {
-    return <Element tag="span" {...this.props} />
-  }
+export function Inline(props) {
+  return <Element tag="span" {...props} />
 }
 
-export class InlineBlock {
-  static displayName = 'InlineBlock';
-  render() {
-    return <Element display="inline-block" verticalAlign="middle" {...this.props} />
-  }
+export function InlineBlock(props) {
+  return <Element display="inline-block" verticalAlign="middle" {...props} />
 }
